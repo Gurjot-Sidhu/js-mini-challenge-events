d02c59a6191cffc0076a57ed9cf6c99aa21e0823
Gurjot-Sidhu <gurjotsidhu4@gmail.com>
2020-03-12 16:09:40 -0400

    delev complete

--- index.js
+++ index.js
@@ -32,15 +32,66 @@ PLAYERS.forEach(renderPlayer)
 
 
 /***** Deliverable 1 *****/
-function toggleColor(element) {
-  if (element.style.color === "red") {
-    element.style.color = "black"
+
+
+function toggleColor() {
+  header = document.querySelector("h1#header")
+  if (header.style.color === "red") {
+    header.style.color = "black"
   } else {
-    element.style.color = "red"
+    header.style.color = "red"
   }
 }
 
+document.addEventListener("click",toggleColor)
+
 
 /***** Deliverable 2 *****/
+playerform = document.querySelector("#new-player-form")
 
+playerform.addEventListener("submit",handleformsubmit)
+
+function handleformsubmit(event) {
+
+  event.preventDefault()
+  const form = event.target
+
+  const playernumber = form["number"].value;
+  const playername = form["name"].value;
+  const playernick = form["nickname"].value;
+  const playerphoto = form["photo"].value;
+
+  const player = {
+    number: playernumber,
+    name: playername,
+    nickname: playernick,
+    photo: playerphoto
+  }
+  renderOnePlayer(player)
+
+  form.reset()
+}
+
+function renderOnePlayer(player){
+  const playerCont = document.querySelector(".player-container")
+  const outerli = document.createElement('li')
+  outerli.className = "player"
+  
+  outerli.innerHTML = `<h3>${player.name} <em>(${player.nickname})</em>
+  </h3>
+  <img src = "${player.photo}" alt=${player.name}>
+  <p class="likes"> 1000 likes</p>
+  <button class="like-button">❤️</button>
+  `
+  playerCont.append(outerli)
+
+}
 /***** Deliverable 3 *****/
+  const playerCont = document.querySelector(".player-container")
+  playerCont.addEventListener("click", e =>{
+    if(e.target.className === "like-button"){
+      const player = e.target.closest(".player")
+      const likescount = player.querySelector(".likes")
+      likescount.textContent = (parseInt(likescount.textContent) + 1) + " likes"
+    }
+  })
